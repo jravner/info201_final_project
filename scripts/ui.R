d260377ba22c43d6c05e11aa17e0e5603e7c9a8b
Steven Bianco <stevbian@uw.edu>
2018-05-29 19:23:11 -0700

    completed UI for price chart

--- scripts/ui.R
+++ scripts/ui.R
@@ -17,7 +17,11 @@ my_ui <- navbarPage(
                textInput("start_date", label = h3("Date input (YYYY-MM-DD)"),
                          value = "NA"),
                textInput("end_date", label = h3("Date input (YYYY-MM-DD)"),
-                         value = Sys.Date())
+                         value = Sys.Date()),
+               selectInput("y-axis", label = h3("y-axis"), 
+                           choices = list("Choice 1" = "price", "Choice 2" = "returns"), 
+                           selected = 1),
+               textInput("ticker", label = h3("Choose ticker symbol (XXXX)"), value = "AAPL")
              ),
              
              # Create a spot for the barplot
